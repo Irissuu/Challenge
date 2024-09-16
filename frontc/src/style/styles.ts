bdf50d978f9c833a29c5d1978045324b835461b8
irissuu <iriswinx2@gmail.com>
2024-09-16 19:28:37 -0300

    trabalhando no cabeçalho

--- frontc/src/style/styles.ts
+++ frontc/src/style/styles.ts
@@ -1,3 +1,22 @@
 import styled from 'styled-components';
 import { createGlobalStyle } from 'styled-components';
 
+// Cabeçalho
+export const CabecalhoContainer = styled.header`
+  background-color: #9AC4E3;
+  padding: 20px;
+  text-align: left;
+  color: white;
+  font-size: 30px;
+  margin-bottom: 5px;
+  
+  img{
+    width: 70px;
+    height: 70px;
+    border-radius: 10px;
+  }
+
+  .loginho{
+    text-align: left;
+  }
+`
