751700f3c30674fb43341b81cc87eeac111a953d
nyxtais <nyxtais@gmail.com>
2024-10-28 16:47:21 -0300

    arquivo nextjs

--- frontc/src/components/Rodape/Rodape.tsx
+++
@@ -1,9 +0,0 @@
-import { RodapeContainer } from '../../style/styles';
-
-export default function Rodape() {
-  return (
-    <RodapeContainer>
-      <p>&copy; 2024 NovaApex.</p>
-    </RodapeContainer>
-  );
-}
